5c7ed55c8d73728ca773b872b4d7056e5d86eca1
ralocorvette <raul.a.gonzalez2507@gmail.com>
2023-07-30 09:59:10 -0500

    updated model py file

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 # Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
